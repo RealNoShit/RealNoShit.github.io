31916385c46466fc5e93b166b137c5426720a12c
RealNoShit <ctwk2001@gmail.com>
2025-10-23 20:00:39 +0200

    new rainbow

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const bouncers = [
+  { el: document.getElementById('bouncer1'), x: 100, y: 100, dx: 3, dy: 2 },
+  { el: document.getElementById('bouncer2'), x: 300, y: 200, dx: -2, dy: 3 },
+];
+
+function moveBouncers() {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+
+  bouncers.forEach(b => {
+    b.x += b.dx;
+    b.y += b.dy;
+
+    if (b.x <= 0 || b.x + b.el.width >= w) b.dx *= -1;
+    if (b.y <= 0 || b.y + b.el.height >= h) b.dy *= -1;
+
+    b.el.style.left = b.x + 'px';
+    b.el.style.top = b.y + 'px';
+  });
+
+  requestAnimationFrame(moveBouncers);
+}
+
+moveBouncers();
